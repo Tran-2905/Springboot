135cb7ef629d39424ad962bb19486359ee6662a3
Royce Tran <78252279+Tran-2905@users.noreply.github.com>
2025-06-05 01:51:57 +0700

    complete angular

--- ahopapp-angular/src/app/app.module.ts
+++ ahopapp-angular/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 import { OrderComponent } from './order/order.component';
+import { OrderConfirmedComponent } from './order-confirmed/order-confirmed.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { OrderComponent } from './order/order.component';
     HeaderComponent,
     FooterComponent,
     DetailProductComponent,
-    OrderComponent
+    OrderComponent,
+    OrderConfirmedComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
@@ -21,7 +27,10 @@ import { OrderComponent } from './order/order.component';
   providers: [],
   bootstrap: [
     // HomeComponent
-    DetailProductComponent
+    // OrderComponent,
+    // OrderConfirmedComponent,
+    // LoginComponent
+    RegisterComponent
   ]
 })
 export class AppModule { }
