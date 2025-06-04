d321418d0d11eaa791fb86c1bcd82347c34a364a
Royce Tran <78252279+Tran-2905@users.noreply.github.com>
2025-06-04 23:17:59 +0700

    order detail angular

---
+++ ahopapp-angular/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [
+    // HomeComponent
+    DetailProductComponent
+  ]
+})
+export class AppModule { }
